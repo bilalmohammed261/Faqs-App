e7436d397eacfec5c5d447e110fdbab1b37e8e5a
Bilal <bilalmohammed261@gmail.com>
2024-07-05 16:53:50 +0000

    started coding the app

--- src/components/FaqItem/index.js
+++ src/components/FaqItem/index.js
@@ -5,6 +5,13 @@ const FaqItem = props => {
   return (
     <div>
       <h1>{questionText}</h1>
+      <button type="button">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/faqs-plus-icon-img.png"
+          alt="plus"
+        />
+      </button>
+      <hr />
       <p>{answerText}</p>
     </div>
   )
