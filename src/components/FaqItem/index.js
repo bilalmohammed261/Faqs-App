b82941e747c64f66b0dee0bc288160d7047aef67
Bilal <bilalmohammed261@gmail.com>
2024-07-10 04:24:10 +0000

    added css

--- src/components/FaqItem/index.js
+++ src/components/FaqItem/index.js
@@ -1,29 +1,65 @@
-// Write your code here.
-const FaqItem = props => {
-  const {faqDetails, toggleIsFaqClicked} = props
-  const {questionText, answerText, isClicked, id} = faqDetails
-  const imgUrl = isClicked
-    ? 'https://assets.ccbp.in/frontend/react-js/faqs-minus-icon-img.png'
-    : 'https://assets.ccbp.in/frontend/react-js/faqs-plus-icon-img.png'
-  const altText = isClicked ? 'minus' : 'plus'
-  const faqAnswerText = isClicked ? answerText : ''
+import {Component} from 'react'
 
-  const onClickFaq = () => {
-    console.log(id)
+import './index.css'
 
-    toggleIsFaqClicked(id)
+const PLUS_IMAGE =
+  'https://assets.ccbp.in/frontend/react-js/faqs-plus-icon-img.png'
+const MINUS_IMAGE =
+  'https://assets.ccbp.in/frontend/react-js/faqs-minus-icon-img.png'
+
+class FaqItem extends Component {
+  state = {
+    isActive: false,
   }
 
-  return (
-    <li>
-      <h1>{questionText}</h1>
-      <button type="button" onClick={onClickFaq}>
-        <img src={imgUrl} alt={altText} />
+  renderAnswer = () => {
+    const {faqDetails} = this.props
+    const {answerText} = faqDetails
+    const {isActive} = this.state
+
+    if (isActive) {
+      return (
+        <div>
+          <hr className="horizontal-line" />
+          <p className="answer">{answerText}</p>
+        </div>
+      )
+    }
+    return null
+  }
+
+  onToggleIsActive = () => {
+    this.setState(prevState => ({
+      isActive: !prevState.isActive,
+    }))
+  }
+
+  renderActiveImage = () => {
+    const {isActive} = this.state
+    const image = isActive ? MINUS_IMAGE : PLUS_IMAGE
+    const altText = isActive ? 'minus' : 'plus'
+
+    return (
+      <button className="button" type="button" onClick={this.onToggleIsActive}>
+        <img className="image" src={image} alt={altText} />
       </button>
-      <hr />
-      <p>{faqAnswerText}</p>
-    </li>
-  )
+    )
+  }
+
+  render() {
+    const {faqDetails} = this.props
+    const {questionText} = faqDetails
+
+    return (
+      <li className="faq-item">
+        <div className="question-container">
+          <h1 className="question">{questionText}</h1>
+          {this.renderActiveImage()}
+        </div>
+        {this.renderAnswer()}
+      </li>
+    )
+  }
 }
 
 export default FaqItem
--- src/components/Faqs/index.js
+++ src/components/Faqs/index.js
@@ -1,47 +1,22 @@
-// Write your code here.
-import {Component} from 'react'
 import FaqItem from '../FaqItem'
 
-class Faqs extends Component {
-  constructor(props) {
-    super(props)
-    const updatedFaqsList = props.faqsList.map(faq => ({
-      ...faq,
-      isClicked: false,
-    }))
-    this.state = {
-      faqsList: updatedFaqsList,
-    }
-  }
+import './index.css'
 
-  toggleIsFaqClicked = id => {
-    this.setState(prevState => ({
-      faqsList: prevState.faqsList.map(eachFaq => {
-        if (id === eachFaq.id) {
-          return {...eachFaq, isClicked: !eachFaq.isClicked}
-        }
-        return eachFaq
-      }),
-    }))
-  }
+const Faqs = props => {
+  const {faqsList} = props
 
-  render() {
-    const {faqsList} = this.state
-    return (
-      <div className="app-container">
-        <h1>FAQs</h1>
-        <ul>
+  return (
+    <div className="app-container">
+      <div className="faqs-container">
+        <h1 className="heading">FAQs</h1>
+        <ul className="faqs-list">
           {faqsList.map(eachFaq => (
-            <FaqItem
-              key={eachFaq.id}
-              faqDetails={eachFaq}
-              toggleIsFaqClicked={this.toggleIsFaqClicked}
-            />
+            <FaqItem key={eachFaq.id} faqDetails={eachFaq} />
           ))}
         </ul>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Faqs
